6cb79a365eec52d1b2c67cf68aae72ddfcdabf02
nellierosenfield <nellierosenfield@gmail.com>
2023-09-24 12:42:38 -0400

    Add new SEO analyzer project to main and README

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ List of mini Web Scraping projects for practice:
 2. scrape_to_csv.py -> web scraping project that GETs page contents from books.toscrape and writes them to new csv file
 3. scrape_price_notifier.py -> web scraping project that checks once a day if a price has dropped and, if so, sends an email notification
 4. scrape_quotes_with_streamlit.py -> scraping quotes.toscrape using Streamlit and producing option to generate csv
+5. seo_analyzer_with_pybs4.py -> SEO analyzer using python and bs4 and exports to csv
---
+++ seo_analysis_pybs4.py/seo_analyzer_with_pybs4.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+# For this analyzer, we will be checking for the following:
+#     - Are there any warnings?
+#     - Is there a title and meta description?
+#     - Are there are enough header tags, especially h1?
+#     - Do images have atl tags for accessibility?
+#     - Do the keywords match or relate to the page title?
+
+url = "https://pythonology.eu/what-is-syntax-in-programming-and-linguistics/"
+response = requests.get(url).text
+
+soup = BeautifulSoup(response, 'html.parser')
+
+not_found = [] # What's not found on the site
+found = [] # Items present on the site
+keywords = [] # if present, keywords for optimizing
+
+# Is there a title and meta description?
+title = soup.find('title').text
+if title:
+    found.append(f"Title: {title} (size {len(title)})")
+else:
+    not_found.append('Title not found! Please add a title to your site')
+
+meta_desc = soup.find('meta', attrs = {'name': 'description'})['content']
+if meta_desc:
+    found.append(f"Meta Description: {meta_desc}")
+else:
+    not_found.append('Meta description not found! Please add a meta description to your site.')
+
+# Are there are enough header tags, especially h1?
+headings = ['h1', 'h2', 'h3']
+heading_tags = []
+for heading in soup.find_all(headings):
+    found.append(f"{heading.name} --> {heading.text.strip()}")
+    heading_tags.append(heading.name)
+
+if 'h1' not in heading_tags:
+    not_found.append('No h1 tags found! Please add at least one (1) h1 tag to your site.')
+
+# Do images have atl tags for accessibility?
+for image in soup.find_all('img', alt = ''):
+    not_found.append(f"Warning! This image does not contain an alt: {image}")
