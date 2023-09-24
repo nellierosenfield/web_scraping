4cfa1c2e92b6ebe232137613b2f191f2d7d6e126
nellierosenfield <nellierosenfield@gmail.com>
2023-09-24 00:08:19 -0400

    Scrape page and display quotes

--- streamlit_quotes/scrape_quotes_with_streamlit.py
+++ streamlit_quotes/scrape_quotes_with_streamlit.py
@@ -3,4 +3,24 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.selectbox('Choose a topic', ['love', 'humor', 'life', 'books'])
+# List of tags we'll focus on and creates selectbox in st portal
+tag = st.selectbox('Choose a topic', ['love', 'humor', 'life', 'books'])
+
+# Url we are scrapping
+url =f"https://quotes.toscrape.com/tag/{tag}/"
+response = requests.get(url)
+
+content = BeautifulSoup(response.content, 'html.parser')
+quotes = content.find_all('div', class_ = 'quote')
+
+# From each quote container, we get the quote text, author and author page url
+for quote in quotes:
+    text = quote.find('span', class_ = 'text').text
+    author = quote.find('small', class_ = 'author').text
+    link = f"https://quotes.toscrape.com{quote.find('a')['href']}"
+
+    # Adds nice green background to displayed text
+    st.success(text)
+
+    # Hyperlinks author name to author page link
+    st.markdown(f"<a href=\"{link}\">{author}</a>", unsafe_allow_html = True)
